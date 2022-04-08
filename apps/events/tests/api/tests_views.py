8bfd4ba8a3693430fca35fc9f33e90549e478d17
HandBoy <handersonmedeiros@gmail.com>
2022-04-08 13:50:44 -0300

    test: try create event with invalid timestamp

--- apps/events/tests/api/tests_views.py
+++ apps/events/tests/api/tests_views.py
@@ -63,6 +63,25 @@ class PostEventsAPITestCase(APITestCase):
         # Then
         self.assertEqual(response.status_code, 400)
 
+    def test_post_event_with_invalid_timestamp(self):
+        # Give
+        data = {
+            "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
+            "category": "page interaction",
+            "name": "pageview",
+            "data": {
+                "host": "www.consumeraffairs.com",
+                "path": "/"
+            },
+            "timestamp": "01/01/2021 09:15:27.243860"
+        }
+        # When
+        response = self.client.post(
+            self.url, data=data, format="json", **self.headers
+        )
+        # Then
+        self.assertEqual(response.status_code, 400)
+
     def test_post_event(self):
         # Give
         data = {
