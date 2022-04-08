a3fd5106a75f207cec999377c0d8afe81fad93b4
HandBoy <handersonmedeiros@gmail.com>
2022-04-08 13:15:02 -0300

    test: save an event

---
+++ apps/events/tests/api/tests_views.py
@@ -0,0 +1,84 @@
+import uuid
+
+from apps.events.models import Application
+from rest_framework.test import APITestCase
+
+
+def generata_application():
+    return Application.objects.create(
+        name="app_01",
+        uuid=uuid.uuid4()
+    )
+
+
+class PostEventsAPITestCase(APITestCase):
+    def setUp(self):
+        self.url = "/api/v1/events/"
+        self.headers = {
+            "HTTP_APPLICATION": generata_application().uuid,
+        }
+
+    def test_post_event_without_application(self):
+        # Give
+        data = {
+            "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
+            "category": "page interaction",
+            "name": "pageview",
+            "data": {
+                "host": "www.consumeraffairs.com",
+                "path": "/"
+            },
+            "timestamp": "2021-01-01 09:15:27.243860"
+        }
+        # When
+        response = self.client.post(self.url, json=data)
+
+        # Then
+        self.assertEqual(response.status_code, 400)
+    
+    def test_post_event_application_does_not_exists(self):
+        # Give
+        data = {
+            "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
+            "category": "page interaction",
+            "name": "pageview",
+            "data": {
+                "host": "www.consumeraffairs.com",
+                "path": "/"
+            },
+            "timestamp": "2021-01-01 09:15:27.243860"
+        }
+        # When
+        response = self.client.post(self.url, json=data)
+
+        # Then
+        self.assertEqual(response.status_code, 400)    
+    
+    def test_post_event_without_requirements_fields(self):
+        # Give
+        data = {}
+        # When
+        response = self.client.post(self.url, json=data)
+
+        # Then
+        self.assertEqual(response.status_code, 400)
+
+    def test_post_event(self):
+        # Give
+        data = {
+            "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
+            "category": "page interaction",
+            "name": "pageview",
+            "data": {
+                "host": "www.consumeraffairs.com",
+                "path": "/"
+            },
+            "timestamp": "2021-01-01 09:15:27.243860"
+        }
+        # When
+        response = self.client.post(
+            self.url, data=data, format="json", **self.headers
+        )
+        # Then
+        self.assertEqual(response.status_code, 201)
+
